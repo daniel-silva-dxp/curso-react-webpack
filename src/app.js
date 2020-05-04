bc44a0cbb9b769c59aea5d42f5a84037843b1fe3
Daniel Silva <dani3l.dxp@gmail.com>
2020-05-04 15:35:55 -0300

    feat: atributos do HTML

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import Title from './title';
 const App = React.createClass({
 	render: function() {
 		return (
-			<div>
+			<div className="container">
 				<Title name="Daniel Silva" />
 			</div>
 		);
